c05b66fc8b258f40ba605839b835e2cc952e69cd
mrbox <mrbox@DESKTOP-BAO1JU4>
2021-04-13 13:09:07 +0300

    Добавьте файлы проекта.

---
+++ Game/Window/GameWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Game.Page;
+
+namespace Game.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            FrameMain.Navigate(new TitleScrin());
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveWindow saveWindow = new SaveWindow();
+            this.Close();
+            saveWindow.Show();
+
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            SaveWindow saveWindow = new SaveWindow();
+            this.Close();
+            saveWindow.Show();
+        }
+    }
+}
---
+++ Game/Window/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Game.Window;
+
+
+namespace Game
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            this.Close();
+            gameWindow.Show();
+
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadWindow loadWindow = new LoadWindow();
+            this.Close();
+            loadWindow.Show();
+            
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
